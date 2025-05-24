c75c6f1971ccf8552c82a4b956b861ba23b1b89a
Prasan <prasan@internshala.com>
2025-05-24 15:04:56 +0530

    main.go updated

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"urlshort/routes"
 	"urlshort/db/migrations"
+	"urlshort/routes"
 )
 
 func main() {
-	fmt.Println("Hello, Go!")
-	migrations.RunMigrations();
+	var runMigrations bool
+
+	flag.BoolVar(&runMigrations, "migrate", false, "Set this flag to run migrations")
+	flag.Parse()
+
+	if runMigrations {
+		migrations.RunMigrations()
+	} else {
+		fmt.Print("Skipping migrations")
+
+	}
 	mux := routes.RegisterRoutes()
-	log.Fatal(http.ListenAndServe(":8081", mux))
+	log.Fatal(http.ListenAndServe(":8082", mux))
 }
